f9b63cd56920fc3ae58fbe805d9144c25834df65
Mahir <mkojak75@gmail.com>
2025-01-28 16:52:28 +0100

    starter

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <section>
+        <h1>Hallo Mein Name ist Muzaffer</h1>
+    </section>
+  )
+}
+
+export default HeroSection
