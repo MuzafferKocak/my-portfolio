5edde5029c5dc45b0078fb46ae6e3e0f902f129c
Mahir <mkojak75@gmail.com>
2025-02-04 10:05:14 +0100

    sendmail

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,23 +1,60 @@
 // import { EmailTemplate } from '../../../components/EmailTemplate';
-import { Resend } from 'resend';
+import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
+const fromEmail = process.env.FROM_EMAIL;
+const toEmail = process.env.TO_EMAIL;
 
-export async function POST() {
+export async function POST(req) {
   try {
-    const { data, error } = await resend.emails.send({
-      from: 'Acme <onboarding@resend.dev>',
-      to: ['mkojak75@gmail.com'],
-      subject: 'Hello world',
-      react: EmailTemplate({ firstName: 'John' }),
-    });
+    const body = await req.json();
+    const { email, subject, message } = body;
 
-    if (error) {
-      return Response.json({ error }, { status: 500 });
+    const { data: userMessageData, error: userMessageError } =
+      await resend.emails.send({
+        from: fromEmail,
+        to: [toEmail],
+        subject: subject,
+        react: (
+          <>
+            <h1>{subject}</h1>
+            <p>New Message Received!</p>
+            <p>From: {email}</p>
+            <p>Message: {message}</p>
+          </>
+        ),
+      });
+    if (userMessageError) {
+      return new Response(JSON.stringify({ error: userMessageError }), {
+        status: 500,
+      });
     }
 
-    return Response.json(data);
+    //* Send a thank-you email to the user
+    // const { data: thankYouData, error: thankYouError } =
+    //   await resend.emails.send({
+    //     from: fromEmail,
+    //     to: [email],
+    //     subject: "We Received Your Message!",
+    //     react: (
+    //       <>
+    //         <h1>Thank You!</h1>
+    //         <p>Hi {email},</p>
+    //         <p>We have received your message and will get back to you soon.</p>
+    //       </>
+    //     ),
+    //   });
+
+    // if (thankYouError) {
+    //   return new Response(JSON.stringify({ error: thankYouError }), {
+    //     status: 500,
+    //   });
+    // }
+
+    return new Response(JSON.stringify({ success: true }), { status: 200 });
   } catch (error) {
-    return Response.json({ error }, { status: 500 });
+    return new Response(JSON.stringify({ error: error.message }), {
+      status: 500,
+    });
   }
 }
