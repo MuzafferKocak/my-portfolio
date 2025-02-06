18039e00a94dd01879095d11aa2902f3d3f20d06
Mahir <mkojak75@gmail.com>
2025-02-07 00:06:11 +0100

    projectCard

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -12,21 +12,23 @@ const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl }) => {
         <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#121212] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-70 transition-all duration-600">
           <Link
             href={gitUrl}
+            target="_blank"
             className="h-14 w-14 border-2 mr-4 relative rounded-full border-[#adb7be] hover:border-white group/link"
           >
             <CodeBracketIcon className="absolute h-10 w-10 text-[#adb7be] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer group-hover/link:text-white" />
           </Link>
           <Link
             href={previewUrl}
+            target="_blank"
             className="h-14 w-14 border-2 relative rounded-full border-[#adb7be] hover:border-white group/link"
           >
             <EyeIcon className="absolute h-10 w-10 text-[#adb7be] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer group-hover/link:text-white" />
           </Link>
         </div>
       </div>
-      <div className="text-white rounded-b-xl bg-[#121212] py-6 px-4">
+      <div className="text-white rounded-b-xl bg-[#121212] py-6 px-4 flex flex-col h-[360px]">
         <h5 className="text-xl font-semibold mb-2 ">{title}</h5>
-        <p className="text-[#adb7be]">{description}</p>
+        <p className="text-[#adb7be] flex-grow">{description}</p>
       </div>
     </div>
   );
--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -1,53 +1,93 @@
-import React from "react";
+"use client"
+import React, { useRef } from "react";
 import ProjectCard from "./ProjectCard";
+import { motion, useInView } from "framer-motion";
 
 const projectsData = [
   {
     id: 1,
-    title: "Project 1",
-    description: "Project 1 description",
+    title: "Milestone-Blog-App",
+    description: "Blog-App is a modern and fully responsive blogging platform built with React, MUI, and Redux Toolkit for seamless state management. The backend is custom-developed using Node.js, Express, and MongoDB, ensuring high performance and scalability. Users can create, edit, and manage blog posts efficiently while enjoying a sleek, intuitive UI. With optimized SEO and a dynamic architecture, Blog-App delivers a fast and engaging experience across all devices.",
     image: "/images/projects/myBlog.png",
-    tag: ["All", "Web"],
-    gitUrl: "/",
-    previewUrl: "/",
+    gitUrl: "https://github.com/MuzafferKocak/milestone-blog-app",
+    previewUrl: "https://blog-app-murex-two.vercel.app/",
   },
   {
     id: 2,
-    title: "Project 2",
-    description: "Project 2 description",
+    title: "Movie-App",
+    description: "Movie-App is a modern movie application built with React, Tailwind CSS, Firebase, React Router, and Context API. Users can explore popular movies, add them to favorites, and access detailed information. It offers a fast, sleek, and user-friendly interface.",
     image: "/images/projects/movie-app.png",
-    tag: ["All", "Web"],
-    gitUrl: "/",
-    previewUrl: "/",
+    gitUrl: "https://github.com/MuzafferKocak/movie-app",
+    previewUrl: "https://movie-app-omega-woad.vercel.app/",
   },
   {
     id: 3,
-    title: "Project 3",
-    description: "Project 3 description",
+    title: "Netflix-App",
+    description: "Netflix-App is a modern movie and TV show platform built with Next.js and Tailwind CSS. Users can explore popular content, access details, and enjoy a seamless user experience. With its fast, dynamic, and responsive design, it delivers excellent performance on all devices.",
     image: "/images/projects/netflix-app.png",
-    tag: ["All", "Web"],
-    gitUrl: "/",
-    previewUrl: "/",
+    gitUrl: "https://github.com/MuzafferKocak/netflix-app",
+    previewUrl: "https://netflix-app-u4o2.vercel.app/",
+  },
+  {
+    id:4,
+    title: "",
+    description: "",
+    image: "",
+    gitUrl: "",
+    previewUrl: "",
+  },
+  {
+    id:5,
+    title: "",
+    description: "",
+    image: "",
+    gitUrl: "",
+    previewUrl: "",
+  },
+  {
+    id:6,
+    title: "",
+    description: "",
+    image: "",
+    gitUrl: "",
+    previewUrl: "",
   },
 ];
 
+
+
 const ProjectsSection = () => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  const cardVariants = {
+    initial: { opacity: 0, y: 50 },
+    animate: { opacity: 1, y: 0 },
+  };
+
   return (
-    <section className="lg:py-14" id="projects" >
-      <h2 className="text-center text-4xl font-bold text-white mb-8 mt-4 md:mb-10">My Projects</h2>
-      <div>
-        {projectsData.map((project) => (
+    <section className="lg:py-16" id="projects" >
+      <h2 className="text-center text-4xl font-bold text-white mb-4 mt-4 md:mb-8">My Projects</h2>
+      <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
+        {projectsData.map((project, index) => (
+          <motion.li
+           key={index}
+            variants={cardVariants}
+            initial="initial"
+            animate={isInView ? "animate" : "initial"}
+            transition={{ duration: 0.3, delay: index * 0.4 }}
+          >
           <ProjectCard
             key={project.id}
             title={project.title}
             description={project.description}
             imgUrl={project.image}
-            tags={project}
             gitUrl={project.gitUrl}
             previewUrl={project.previewUrl}
           />
+          </motion.li>
         ))}
-      </div>
+      </ul>
     </section>
   );
 };
